0e10a19ad68f9d759b244d4a099095b74b98fb65
Frazix12 <71058095+Frazix12@users.noreply.github.com>
2022-05-20 12:42:21 +0600

    Saved on Fri 05/20/2022-12:42:21.61

--- components/fonts.js
+++ components/fonts.js
@@ -1,6 +1,18 @@
 const Fonts = () => (
   <style jsx global>{`
     @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    ::-webkit-scrollbar {
+      width: 8px;
+    }
+    ::-webkit-scrollbar-track {
+      background: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      background: #212329;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+      background: #16171a;
+    }
   `}</style>
 )
 export default Fonts
