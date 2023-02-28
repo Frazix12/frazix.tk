fc46334c86e09536186705fcafdcb2e10fba529a
Frazix12 <frazixgaming22@gmail.com>
2023-02-28 23:24:25 +0600

    Saved on Tue 02/28/2023-23:24:25.05

--- components/spotify.js
+++ components/spotify.js
@@ -1,17 +1,19 @@
 
 import { motion } from 'framer-motion'
-
+const on = false;
 const Spotify = () => {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: -50 }}
-      animate={{ opacity: 1, y: -100 }}
-      transition={{ duration: 0.5, easing: [0, 0.5, 0.28, 0.99] }}
-      className="fixed left-6 -bottom-20 w-[14rem] h-[7rem] hidden lg:flex flex-col items-start justify-start bg-[#212329] rounded-xl pt-2 pb-2 pl-2 pr-2 backdrop-blur-md"
-    >
+  if (on) {
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: -100 }}
+        transition={{ duration: 0.5, easing: [0, 0.5, 0.28, 0.99] }}
+        className="fixed left-6 -bottom-20 w-[14rem] h-[7rem] hidden lg:flex flex-col items-start justify-start bg-[#212329] rounded-xl pt-2 pb-2 pl-2 pr-2 backdrop-blur-md"
+      >
 
-    </motion.div>
-  )
+      </motion.div>
+    )
+  } else return null;
 }
 
 export default Spotify
